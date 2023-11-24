2342918ab78ba246f1e99aafa155d15bd621dbb1
Davi Junior <davimariano@gmail.com>
2023-11-24 18:24:01 -0300

    Correção no index.html correção de bugs e ajustes

--- analytic/settings.py
+++ analytic/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-8maldxucn(#+p-tru4sfb3wvjdl5==$w=nsrr^%ih%)*d-n)(w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.26']
 
 # Application definition
 
