f80140dbf2185ba97e0138eab891ab267b6df645
Davi Junior <davimariano@gmail.com>
2023-11-27 14:16:38 -0300

    Adicionando dominios .com e .br

--- analytic/settings.py
+++ analytic/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-8maldxucn(#+p-tru4sfb3wvjdl5==$w=nsrr^%ih%)*d-n)(w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.analyticcompstat.com', 'www.analyticcompstat.com.br']
 
 
 # Application definition
